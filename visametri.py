41b31e74d1de3542a5a72fc3410433cc4f4b6f18
zaitmambet <z.aitmambet@gmail.com>
2022-07-15 15:17:16 +0200

    minor changes

--- visametri.py
+++ visametri.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 #from webdriver_manager.chrome import ChromeDriverManager
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 def check_earliest_date(earliest_date):
     if earliest_date < '2022-08-25':
@@ -21,6 +22,8 @@ def reserve_appointment():
     service = Service(executable_path=PATH)
     driver = webdriver.Chrome(service=service)
     no_earlier_dates = True
+    current_best_date = '2022-09-16'
+    i = 1
     
     while no_earlier_dates:
         print("=" * 50)
@@ -41,14 +44,31 @@ def reserve_appointment():
         time.sleep(1)
         driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object]']//option[@value='04']").click()
         time.sleep(1)
-        driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object],[object Object]']//option[@value='2']").click()
-        time.sleep(1)
-        driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object]']//option[@value='individual']").click()
-        time.sleep(1)
-        earliest_date = driver.find_element(By.XPATH, "//li[@class='list-group-item']").text
-        if check_earliest_date(earliest_date):
-            print("earlier date found!")
-            no_earlier_dates = False
+        while no_earlier_dates:
+            try:
+                driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object],[object Object]']//option[@value='2']").click()
+                time.sleep(1)
+                driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object]']//option[@value='individual']").click()
+                time.sleep(1)
+                earliest_date = driver.find_element(By.XPATH, "//li[@class='list-group-item']").text
+            except NoSuchElementException as exc:
+                print(exc)
+                earliest_date = current_best_date
+                
+            if check_earliest_date(earliest_date):
+                print("NEW APPOINTMENT DATE FOUND: ", earliest_date, ". Previous best date: ", current_best_date)
+                no_earlier_dates = False
+            elif earliest_date < current_best_date:
+                print("earlier date found: ", earliest_date, ". Previous best date: ", current_best_date)
+                current_best_date = earliest_date
+                driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object],[object Object]']//option[@value='10']").click()
+                print(i, end=" ")
+                time.sleep(5)
+            else:
+                driver.find_element(By.XPATH, "//select[@values='[object Object],[object Object],[object Object],[object Object]']//option[@value='10']").click()
+                print(i, end=" ")
+                time.sleep(5)
+            i += 1
     
     return check_earliest_date(earliest_date)
 
